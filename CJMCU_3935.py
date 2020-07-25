344428656f75305f769047fa9a8d7e029ed3f5b3
Raúl Caro Pastorino <dev@fryntiz.es>
2020-07-26 01:11:17 +0200

    Comienzo a crear métodos y callback para este sensor

--- CJMCU_3935.py
+++ CJMCU_3935.py
@@ -47,13 +47,126 @@ from RPi_AS3935 import RPi_AS3935
 import datetime
 import time
 from AbstractModel import AbstractModel
-from _thread import start_new_thread
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
 
 class CJMCU_3935(AbstractModel):
     table_name = 'table_lightning'
     sensor = None
 
-    def __init__(self, mode_debug=False):
-        self.sensor = RPi_AS3935()
+    def __init__(self, address=0x03, bus=1, mode_debug=False, indoor=True,
+                 pin=25):
+        self.sensor = RPi_AS3935(address, bus)
         self.mode_debug = mode_debug
 
+        self.sensor.set_indoors(indoor)
+        self.sensor.set_noise_floor(0)
+        self.sensor.calibrate(tun_cap=0x0F)
+
+        self.pin = pin
+
+        ## Inicio Callback para en cada detección registrar rayo
+        GPIO.setup(pin, GPIO.IN)
+        GPIO.add_event_detect(pin, GPIO.RISING, callback=self.handle_interrupt)
+
+        print("Waiting for lightning - or at least something that looks like it")
+
+    def handle_interrupt(self, channel):
+        """
+        Función que se ejecuta cuando detecta un rayo para registrarlo
+        en el array de objetos con los datos registrados.
+        :return:
+        """
+        time.sleep(0.003)
+        sensor = self.sensor
+
+        reason = sensor.get_interrupt()
+        if reason == 0x01:
+            print ("Noise level too high - adjusting")
+            sensor.raise_noise_floor()
+        elif reason == 0x04:
+            print ("Disturber detected - masking")
+            sensor.set_mask_disturber(True)
+        elif reason == 0x08:
+            now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            distance = sensor.get_distance()
+            print("We sensed lightning!")
+            print("It was " + str(distance) + "km away. (%s)" % now)
+            print("")
+
+    def get_all_datas(self):
+        """
+        Devuelve un diccionario con todas las lecturas si se han podido tomar.
+        :return:
+        """
+        if self.???:
+            return {
+                "x": self.read_x(),
+            }
+
+        return None
+
+    def tablemodel(self):
+        """
+        Plantea campos como modelo de datos para una base de datos y poder ser
+        tomados desde el exterior.
+        """
+        return {
+            'strike': {
+                'type': 'Numeric',
+                'params': {
+                    'precision': 15,
+                    'asdecimal': True,
+                    'scale': 4
+                },
+                'others': None,
+            },
+            'distance': {
+                'type': 'Numeric',
+                'params': {
+                    'precision': 15,
+                    'asdecimal': True,
+                    'scale': 4
+                },
+                'others': None,
+            },
+            'type': {
+                'type': 'Numeric',
+                'params': {
+                    'precision': 15,
+                    'asdecimal': True,
+                    'scale': 4
+                },
+                'others': None,
+            },
+            'energy': {
+                'type': 'Numeric',
+                'params': {
+                    'precision': 22,
+                    'asdecimal': True,
+                    'scale': 11
+                },
+                'others': None,
+            },
+            'created_at': {
+                'type': 'DateTime',
+                'params': None,
+                'others': {
+                    'default': datetime.datetime.utcnow
+                },
+            },
+        }
+
+    def debug(self):
+        """
+        Función para depurar funcionamiento del modelo proyectando datos por
+        consola.
+        """
+        datas = self.get_all_datas()
+
+        if datas:
+            print('Pintando debug para CJMCU 3935')
+
+            for sensor, data in datas.items():
+                print('Valor del sensor ' + str(sensor) + ': ' + str(data))
